5d5b5157315c66254e44491792037769926eda95
LucasASouzak <lucas.antonio.souza@gmail.com>
2024-04-06 19:32:19 -0300

    erri fui neymar

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 //Classificador de nivel de heroi
 
-let heroName = "Shury"
-let xp = 8500;
+const heroName = "Shury"
+const heroExperience= 2250;
 
 let heroLevel;
-if (heroLevel< 1000) {
-    heroLevel = "Ferro";
+if (heroExperience< 1000) {
+    heroLevel = "Iron";
 }
-else if (heroLevel > 1000 && heroLevel <= 2000){
+else if (heroExperience > 1000 && heroExperience <= 2000){
     heroLevel = "Bronze";
 }
+else if (heroExperience >= 2001 && heroExperience <= 5000){
+    heroLevel =  "Silver";
+}
+else {
+    heroLevel = "God"
+}
 
 
 
-console.log("não funcina")
+console.log(heroLevel)
