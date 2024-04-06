2fd27b2c3e375c8749ded6ff08b0ed8e90e49d6b
LucasASouzak <lucas.antonio.souza@gmail.com>
2024-04-06 20:00:56 -0300

    "Errei fui neymar"

--- index.js
+++ index.js
@@ -4,15 +4,20 @@ const heroName = "Shury"
 const heroExperience= 2250;
 
 let heroLevel;
+
 if (heroExperience< 1000) {
     heroLevel = "Iron";
 }
 else if (heroExperience > 1000 && heroExperience <= 2000){
     heroLevel = "Bronze";
 }
-else if (heroExperience >= 2001 && heroExperience <= 5000){
+else if (heroExperience > 2000 && heroExperience <= 5000){
     heroLevel =  "Silver";
 }
+else if (heroExperience > 5000 && heroExperience <= 7000){
+    heroLevel = "Gold";
+}
+else if 
 else {
     heroLevel = "God"
 }
