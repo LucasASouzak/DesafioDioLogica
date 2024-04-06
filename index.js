d49980c918566c98fa33be0e3b6988279369bd79
LucasASouzak <lucas.antonio.souza@gmail.com>
2024-04-06 18:48:45 -0300

    Teste de codigo JavaScript 2

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log ("hello World");
+//Classificador de nivel de heroi
+
+let heroName = "Shury";
+let xp = 8500;
+
+let heroLevel;
+if (xp < 1000){
+    level ="Ferro"
+}
+return heroLevel;
+console.log("The Hero of name " + heroName + " it's on level: "+ heroLevel + "!");
