ecfacd28b9b136030d4cc8400ea593ead7c44aa0
LucasASouzak <lucas.antonio.souza@gmail.com>
2024-04-07 13:57:23 -0300

    Final

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-//Classificador de nivel de heroi
+//Classificador de nível de herói
 
+// Variável constante para armazenar o nome do herói
 const heroName = "Shury"
+
+// Variável constante para armazenar os pontos de experiência do herói(Necessario alterar manualmente)
 const heroExperience = 13820;
 
+// Variável para armazenar o nível do herói (declarada sem valor inicial)
 let heroLevel;
 
+// Instrução condicional para verificar o nível de experiência
 if (heroExperience< 1000) {
+
+// Atribui "Ferro" ao nivelDoHeroi se a experiência for inferior a 1000
     heroLevel = "Iron";
 }
+
+//semelhante para outras faixas de experiência e atribuições de nível.
 else if (heroExperience > 1000 && heroExperience <= 2000){
     heroLevel = "Bronze";
 }
@@ -26,8 +35,11 @@ else if (heroExperience > 8000 && heroExperience <= 9000){
 else if (heroExperience > 9000 && heroExperience <= 10000){
     heroLevel = "Immortal";
 }
+
+//Classificador geral para experiencia acima de 10000
 else {
     heroLevel = "Radiant";
 }
-    
-console.log(heroLevel)
+
+//Imprime uma mensagem informando o jogador qual seu nível e classificação de herói.
+console.log("The Heroin named " +heroName+ " is at level " + heroLevel+".")
