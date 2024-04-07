8dc612226e0b7acc2fa1db4a7d86a161056aef70
LucasASouzak <lucas.antonio.souza@gmail.com>
2024-04-07 13:34:28 -0300

    Atualização

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //Classificador de nivel de heroi
 
 const heroName = "Shury"
-const heroExperience= 2250;
+const heroExperience = 10001;
 
 let heroLevel;
 
@@ -17,10 +17,17 @@ else if (heroExperience > 2000 && heroExperience <= 5000){
 else if (heroExperience > 5000 && heroExperience <= 7000){
     heroLevel = "Gold";
 }
-else {
-    heroLevel = "God"
+else if (heroExperience > 7000 && heroExperience <= 8000){
+    heroLevel = "Platinum";
 }
-
-
-
+else if (heroExperience > 8000 && heroExperience <= 9000){
+    heroLevel = "Ascending";
+}
+else if (heroExperience > 9000 && heroExperience <= 10000){
+    heroLevel = "Immortal";
+}
+else {
+    heroLevel = "Radiant";
+}
+    
 console.log(heroLevel)
